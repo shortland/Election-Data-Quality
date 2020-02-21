823ab52437dd3e9f07d658e5932347edd3933ce6
shortland <ilankleiman@gmail.com>
2020-02-21 00:12:33 -0500

    feat: mapbox layering & ui buttons for map

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 //import Bootstrap css
@@ -11,11 +11,16 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import StateSelector from './components/StateSelector';
 
 //import React-Mapbox  documanetation here: https://uber.github.io/react-map-gl/docs
-import ReactMapGL from 'react-map-gl';
-import MapGL, { LinearInterpolator, WebMercatorViewport } from 'react-map-gl';
+// import ReactMapGL from 'react-map-gl';
+import MapGL, { Popup, NavigationControl, FullscreenControl, ScaleControl } from 'react-map-gl';
+
+import Pins from './components/map-components/pins';
+import CityInfo from './components/map-components/city-info';
+
+import CITIES from './data/cities.json';
 
 //import Mapbox GL JS (a little different from React-Mapbox), actually not sure we need this
-import Mapbox from 'mapbox-gl';
+// import Mapbox from 'mapbox-gl';
 
 /**
  * bootstrap stuff
@@ -29,26 +34,49 @@ import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 
+const fullscreenControlStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  padding: '10px'
+};
+
+const navStyle = {
+  position: 'absolute',
+  top: 36,
+  left: 0,
+  padding: '10px'
+};
+
+const scaleControlStyle = {
+  position: 'absolute',
+  bottom: 36,
+  left: 0,
+  padding: '10px'
+};
+
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       viewport: {
         width: "100%",
-        height: "95vh",
+        height: "90vh",
         latitude: 37.0902,
         longitude: -95.7129,
-        zoom: 3.5,
+        zoom: 3.3,
       },
-    }
+      popupInfo: null,
+    };
   }
 
-  stateSelect(name){
+  stateSelect(name) {
     let latitude = 0;
     let longitude = 0;
     let zoom;
 
-    switch(name){
+    switch (name) {
       case "NY":
         latitude = 43.2994;
         longitude = -74.2179;
@@ -76,8 +104,31 @@ class App extends Component {
       },
     });
   }
-  
-  render(){
+
+  _onClickMarker = city => {
+    this.setState({ popupInfo: city });
+  };
+
+  _renderPopup() {
+    const { popupInfo } = this.state;
+
+    return (
+      popupInfo && (
+        <Popup
+          tipSize={5}
+          anchor="top"
+          longitude={popupInfo.longitude}
+          latitude={popupInfo.latitude}
+          closeOnClick={false}
+          onClose={() => this.setState({ popupInfo: null })}
+        >
+          <CityInfo info={popupInfo} />
+        </Popup>
+      )
+    );
+  }
+
+  render() {
     return (
       <div className="App">
         <Navbar bg="dark" expand="lg" variant="dark">
@@ -86,8 +137,8 @@ class App extends Component {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="#tmp">Something</Nav.Link>
-              <StateSelector 
-                select_state={(state_abv) => this.stateSelect.bind(this, state_abv)} 
+              <StateSelector
+                select_state={(state_abv) => this.stateSelect.bind(this, state_abv)}
               />
             </Nav>
             <Form inline>
@@ -101,16 +152,34 @@ class App extends Component {
           <Row>
             <Col></Col>
             <Col xs={8}>
-              <ReactMapGL
+              <MapGL
                 {...this.state.viewport}
-                onViewportChange={(viewport => this.setState({viewport: viewport} ))}
-                mapStyle= "mapbox://styles/reedm121/ck6modif70t8r1ilazl4xfw4c"
-                mapboxApiAccessToken="pk.eyJ1IjoicmVlZG0xMjEiLCJhIjoiY2s2bW81dWlnMHJ2djNra2dmbGJvaDByNCJ9.T6GQYdURKxuqQHUTczcZ-g"
-              />
+                onViewportChange={(viewport => this.setState({ viewport: viewport }))}
+                mapStyle="mapbox://styles/shortland/ck6bf8xag0zv81io8o68otfdr/draft"
+                mapboxApiAccessToken="pk.eyJ1Ijoic2hvcnRsYW5kIiwiYSI6ImNqeXVzOWhsbjBpYzczY29hNGZycTlqdXAifQ.B6l-uEqGG-Pw6-quz4eflQ">
+
+                <Pins data={CITIES} onClick={this._onClickMarker} />
+
+                {this._renderPopup()}
+
+                <div style={fullscreenControlStyle}>
+                  <FullscreenControl />
+                </div>
+
+                <div style={navStyle}>
+                  <NavigationControl />
+                </div>
+
+                <div style={scaleControlStyle}>
+                  <ScaleControl />
+                </div>
+
+              </MapGL>
             </Col>
             <Col></Col>
           </Row>
         </div>
+
       </div>
     );
   }
---
+++ src/components/map-components/city-info.js
@@ -0,0 +1,25 @@
+import React, {PureComponent} from 'react';
+
+export default class CityInfo extends PureComponent {
+  render() {
+    const {info} = this.props;
+    const displayName = `${info.city}, ${info.state}`;
+
+    return (
+      <div>
+        <div>
+          {displayName} |{' '}
+          THIS IS TEMPORARY JUST TO SHOW WE CAN CLICK ON STUFF ON MAP
+          <br/>
+          <a
+            target="_new"
+            href={`http://en.wikipedia.org/w/index.php?title=Special:Search&search=${displayName}`}
+          >
+            Wikipedia
+          </a>
+        </div>
+        <img width={240} src={info.image} alt="img"/>
+      </div>
+    );
+  }
+}
