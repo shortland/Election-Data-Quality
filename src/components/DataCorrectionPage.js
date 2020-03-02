24d605c9ea10e784acb2449101f850515f1d03c2
David Lin <david@Davids-MacBook-Pro.local>
2020-03-02 14:21:07 -0500

    add tabs for different data and data correction page

---
+++ src/components/DataCorrectionPage.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
+
+
+
+class DataCorrectionPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    save_changes() {
+        this.dismiss();
+    }
+
+    dismiss() {
+        this.props.callBackLeftSideBar("done");
+    }
+
+
+    render() {
+        //let key = Object.keys(targetData);
+        return (
+            <div className="data_correction_page">
+                <h4>Data Correction Form</h4>
+                <Form>
+                    <Form.Group controlId="data_correction.target_data">
+                        <Form.Label>Data for correction</Form.Label>
+                        <Form.Control as="select">
+                            <option>Demographic</option>
+                            <option>Election</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="data_correction.value">
+                        <Form.Label>Changed Value</Form.Label>
+                        <Form.Control type="value" placeholder="Please enter corrected data" />
+                    </Form.Group>
+                </Form>
+                <div className="data_correction_Btns">
+                    <Button variant="outline-primary" id="data_change" onClick={() => { this.save_changes() }}>Change</Button>
+                    <Button variant="outline-primary" id="data_change_cancel" onClick={() => { this.dismiss() }}>Cancel</Button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DataCorrectionPage;
--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -4,13 +4,19 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Table from 'react-bootstrap/Table';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
 
 import ElectionDisplayBar from '../components/ElectionDisplayBar';
 import DemographicsTable from '../components/DemographicsTable';
+import DataCorrectionPage from '../components/DataCorrectionPage';
 
 class LeftSidebar extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            mode: "data_display"
+        }
     }
 
     createList = () => {
@@ -29,18 +35,52 @@ class LeftSidebar extends Component {
         return list;
     }
 
+    //new for data correction page
+    data_correct_selected() {
+        this.setState({ mode: "data_correction" });
+    }
+
+    get_data_correction_page_status = (page_status) => {
+        if (page_status === "done") {
+            this.setState({ mode: "data_display" });
+        }
+    }
+
     render() {
+        const { mode } = this.state;
         const list = this.createList();
         const feature = this.props.selected;
 
         if (list.length > 0) {
-            return (
-                <div>
-                    {list}
-                    <ElectionDisplayBar />
-                    <DemographicsTable />
-                </div>
-            );
+            if (mode === "data_display") {
+                return (
+                    <div>
+                        <Tabs defaultActiveKey="general_display" id="left_side_bar_data_tabs">
+                            <Tab eventKey="general_display" title="General Data">
+                                {list}
+                            </Tab>
+                            <Tab eventKey="demographic_display" title="DemographicsTable">
+                                <DemographicsTable />
+                            </Tab>
+                            <Tab eventKey="election_display" title="ElectionBar">
+                                <ElectionDisplayBar />
+                            </Tab>
+                        </Tabs>
+                        <div className="data_correction">
+                            <button id="data_correction_btn" onClick={() => this.data_correct_selected()}>Correct Data Request</button>
+                        </div>
+                    </div >
+                );
+            }
+            else if (mode === "data_correction") {
+                return (
+                    <div>
+                        <div>
+                            <DataCorrectionPage callBackLeftSideBar={this.get_data_correction_page_status} />
+                        </div >
+                    </div>
+                );
+            }
         }
         return (
             <div>
