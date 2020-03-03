9cf9631bd0ff1d408315d78960f4d096529e2c99
Reed Gantz <reedm121@gmail.com>
2020-03-03 12:36:03 -0500

    better fake demographic data

--- src/components/DemographicsTable.js
+++ src/components/DemographicsTable.js
@@ -18,29 +18,35 @@ class DemographicsTable extends PureComponent {
                         <tr>
                             <th>White</th>
                             <td>112</td>
-                            <td>0%</td>
+                            <td>44%</td>
                         </tr>
                         <tr>
                             <th>Black</th>
                             <td>55</td>
-                            <td>0</td>
+                            <td>22%</td>
                         </tr>
                         <tr>
                             <th>Hispanic</th>
-                            <td>86</td>
-                            <td>0</td>
+                            <td>80</td>
+                            <td>32%</td>
                         </tr>
                         <tr>
                             <th>Asian</th>
-                            <td>0</td>
-                            <td>0</td>
+                            <td>6</td>
+                            <td>2%</td>
                         </tr>
                         <tr>
                             <th>Other</th>
                             <td>0</td>
-                            <td>0</td>
+                            <td>0%</td>
                         </tr>
                     </tbody>
+                    <tfoot>
+                        <tr>
+                            <th colSpan="2">Total Population</th>
+                            <td>253</td>
+                        </tr>
+                    </tfoot>
                 </Table>
             </div>
         );
