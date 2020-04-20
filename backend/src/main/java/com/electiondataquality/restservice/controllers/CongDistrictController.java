355c43238f0f8b3a7f3bc74ed214ef67b660b2bd
DavidLSC <b25210908@gmail.com>
2020-04-19 20:17:15 -0400

    fix CongDistrictController

--- backend/src/main/java/com/electiondataquality/restservice/controllers/CongDistrictController.java
+++ backend/src/main/java/com/electiondataquality/restservice/controllers/CongDistrictController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.electiondataquality.restservice.RestServiceApplication;
 import com.electiondataquality.restservice.managers.StateManager;
