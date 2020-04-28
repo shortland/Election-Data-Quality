9067aba0f4e9cb5b9c6672cdacebbffe0d42120c
shortland <ilankleiman@gmail.com>
2020-04-27 21:57:56 -0400

    feat: impl. Comment & Error entity table equivalents

---
+++ backend/src/main/java/com/electiondataquality/jpa/tables/CommentTable.java
@@ -0,0 +1,86 @@
+package com.electiondataquality.jpa.tables;
+
+import java.util.Date;
+
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "comments")
+public class CommentTable {
+
+    @Id
+    @Column(name = "idn")
+    private int commentId;
+
+    @Column(name = "error_idn")
+    private int errorId;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "created")
+    private Date created;
+
+    @Column(name = "valid")
+    private int valid;
+
+    public CommentTable() {
+    }
+
+    public CommentTable(int commentId, int errorId, String commentText, Date created, int valid) {
+        this.commentId = commentId;
+        this.errorId = errorId;
+        this.text = commentText;
+        this.created = created;
+        this.valid = valid;
+    }
+
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
+
+    public int getErrorId() {
+        return errorId;
+    }
+
+    public void setErrorId(int errorId) {
+        this.errorId = errorId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public int getValid() {
+        return valid;
+    }
+
+    public void setValid(int valid) {
+        this.valid = valid;
+    }
+
+    @Override
+    public String toString() {
+        return "CommentTable [commentId=" + commentId + ", created=" + created + ", errorId=" + errorId + ", text="
+                + text + ", valid=" + valid + "]";
+    }
+}
