c79fafcdca9ebc7e277c3720fdcce86615badb4b
DavidLSC <b25210908@gmail.com>
2020-05-18 15:39:54 -0400

    add enclosed error type

--- backend/src/main/java/com/electiondataquality/features/precinct/error/enums/ERROR_TYPE.java
+++ backend/src/main/java/com/electiondataquality/features/precinct/error/enums/ERROR_TYPE.java
@@ -1,5 +1,5 @@
 package com.electiondataquality.features.precinct.error.enums;
 
 public enum ERROR_TYPE {
-    OVERLAP, GHOST, NO_VOTERS, NO_DEMOGRAPHICS, INTERSECTING;
+    OVERLAP, GHOST, NO_VOTERS, NO_DEMOGRAPHICS, INTERSECTING, ENCLOSED;
 }
